746b1c4b22911ef751406c12c7b1a061030ce5fa
Rodrigo Regalado <r.regalado6204@gmail.com>
2022-10-02 18:37:27 -0700

    added all clickable items

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(link, left, bottom ) {
+    let character = document.createElement('img')
+    character.src = link 
+    character.style.position = 'fixed'
+    character.style.left = left + 'px'
+    character.style.bottom = bottom + 'px'
+    document.body.append(character)
+    return character 
+}
 
-let pineTree = document.createElement('img')
-pineTree.src = 'assets/pine-tree.png'
-pineTree.style.position = 'fixed'
-pineTree.style.left = '450px'
-pineTree.style.bottom = '200px'
-document.body.append(pineTree)
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+
+function newItem(link, left, bottom) {
+    let character = newImage(link, left, bottom)
+
+    character.addEventListener('click', function() {
+        character.remove()
+    })
+}
+
+newItem('assets/sword.png', 500, 405)
+newItem('assets/staff.png', 600, 100)
+newItem('assets/sheild.png', 165, 185)
